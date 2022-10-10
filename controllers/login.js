312a44b6cbb55fd3035e7d488a92cfee86bde7b5
unknown <prossdube@gmail.com>
2022-10-10 21:52:38 +0200

    new changes

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,8 @@
 const schemas = require('../models/schemas.js')
 const bcrypt = require('bcrypt')
+const passport = require("passport");
+const validator = require("validator");
+//const User = require("../models/User");
 
 exports.getLogin = (req, res) => {
     res.render('login', {title: 'Login', loggedIn: false, error:null})
@@ -47,32 +50,55 @@ exports.postLogin = async (req, res) => {
     } else {
         res.render('login', {title:'Login', loggedIn: false, error: 'Invalid Login'})
     }
-    exports.postSignup = async (req, res) => {
-        let email = req.body.emailInput
-        let pass = req.body.pwdInput
-
-        if (email !== '' && pass !== '' ) {
-            let users = schemas.users
-            let qry = {email:email}
-
-        let userSearch = await users.findOne(qry)
-        .then (async (data) => {
-            if (!data) {
-                let saltRounds = 10
-                let passSalt = await bcrypt.genSalt(saltRounds, async(err, salt) => {
-                    let passHash = await bcrypt.hash(pass, salt, async(err, hash) => {
-                        let acct ={email:email, pwd:hash, level: 'admin'}
-                        let newUser = new schemas.users(acct)
-                        let saveUser = await newUser.save()
-                    })
-                })
-            }
-        })
-        
-        res.render('login', {title:'Login', loggedIn: false, error: 'Please login with your new account'});
-    } else {
-        res.render('new-acct', {title: 'New Account', loggedIn: false, error:'All fields are required. Please check and try again.'})
-    }
-       
 }
-};
+
+    exports.postSignup = (req, res, next) => {
+        const validationErrors = [];
+        if (!validator.isEmail(req.body.email))
+          validationErrors.push({ msg: "Please enter a valid email address." });
+        if (!validator.isLength(req.body.password, { min: 8 }))
+          validationErrors.push({
+            msg: "Password must be at least 8 characters long",
+          });
+        if (req.body.password !== req.body.confirmPassword)
+          validationErrors.push({ msg: "Passwords do not match" });
+      
+        if (validationErrors.length) {
+          req.flash("errors", validationErrors);
+          return res.redirect("../signup");
+        }
+        req.body.email = validator.normalizeEmail(req.body.email, {
+          gmail_remove_dots: false,
+        });
+        const user = new User({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.password,
+          });
+        
+          User.findOne(
+            { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+            (err, existingUser) => {
+              if (err) {
+                return next(err);
+              }
+              if (existingUser) {
+                req.flash("errors", {
+                  msg: "Account with that email address or username already exists.",
+                });
+                return res.redirect("../signup");
+              }
+              user.save((err) => {
+                if (err) {
+                  return next(err);
+                }
+                req.logIn(user, (err) => {
+                  if (err) {
+                    return next(err);
+                  }
+                  res.redirect("/profile");
+                });
+              });
+            }
+          );
+        };
