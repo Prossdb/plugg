5d502521a211058d321b751d1e674f2d3b6e312e
unknown <prossdube@gmail.com>
2022-10-12 21:57:35 +0200

    added cloudinary and passport

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const indexController = require('../controllers/index') 
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
-router.get('/', indexController.getHome)
-router.post('/q', indexController.getSearch)
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,57 +1,63 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('express-flash');
-const logger = require('morgan');
-const cloudinary = require('cloudinary');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
-const connectDB = require('./config/database')
-const indexRouter = require('./routes/index');
-const menuRouter = require('./routes/menu');
-const loginRouter = require('./routes/login');
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
 
-require('dotenv').config({path: './config/.env'})
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
 
-//Passport config
-//require('./config/passport')(passport)
+// Passport config
+require("./config/passport")(passport);
 
-connectDB()
+//Connect To Database
+connectDB();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
+//Using EJS for views
+app.set("view engine", "ejs");
 
-// Sessions
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
-    })
-  )
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-  // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(flash())
+//Use flash messages for errors, info, ect...
+app.use(flash());
 
-  
-app.use('/', indexRouter);
-app.use('/menu', menuRouter);
-app.use('/login', loginRouter);
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})  
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
 
-//cloudinary
-// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//   { public_id: "olympic_flag" }, 
-//   function(error, result) {console.log(result); });
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
