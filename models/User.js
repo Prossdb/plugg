c4ca39a474ca9829502c33d0e2d78760bac38022
Pro- Dube <98844881+Prossdb@users.noreply.github.com>
2022-12-12 22:36:48 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
   password: String,
 });
 
-// Password hash middleware.
+// Password hash middleware.very important
 
 UserSchema.pre("save", function save(next) {
   const user = this;
